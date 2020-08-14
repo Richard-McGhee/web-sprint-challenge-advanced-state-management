4b42ecf4ecd71885b47ef47525fccdc5ae0ab67b
richard-mcghee <richardjmcghee@yahoo.com>
2020-08-14 11:45:42 -0400

    made a base reducer for smurfs data with a basic state, setup provider in the index.js

---
+++ smurfs/src/reducers/smurfReducer.js
@@ -0,0 +1,12 @@
+const initialState = {
+    error: "",
+    loading: false,
+    smurfs: []
+}
+
+export const smurfReducer = (state = initialState, action) => {
+    switch(action.type) {
+        default:
+            return state
+    }
+}
