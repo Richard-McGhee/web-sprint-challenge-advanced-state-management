a997182f7cc87454776fb764d2ce5a87937ad676
richard-mcghee <richardjmcghee@yahoo.com>
2020-08-14 12:29:49 -0400

    got the map working to show all my smurf(s), working on the form now

--- smurfs/src/components/SmurfVillage.js
+++ smurfs/src/components/SmurfVillage.js
@@ -1,7 +1,26 @@
 import React from 'react';
+import { connect } from 'react-redux'
 
-export default function SmurfVillage(props) {
+function SmurfVillage(props) {
     return (
-        <h1>placeholder</h1>
+        <div>
+            {props.error && <h2>Well this isn't good</h2>}
+            {props.loading && <h2>Smurfette is busy...</h2>}
+            {props.smurfs.length > 0 ? (
+                props.smurfs.map((smurf) => (
+                    <div>
+                        <h2 key={smurf.id}>{smurf.name}</h2>
+                        <h3>Height: ...they're all small</h3>
+                        <p>Age: Very old, but more specifically --`'{">"}'` {smurf.age}</p>
+                    </div>
+                    
+                ))
+            ) : null}
+        </div>
     )
 }
+
+const mapProps = (state) => {
+    return state
+}
+export default connect(mapProps, {})(SmurfVillage)
